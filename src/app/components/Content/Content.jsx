33bebb5682e1a021e6d7cf2d3e2df76539cfa4a6
Ajgorek04 <igorblendzinski12@gmail.com>
2024-02-27 22:20:16 +0100

    adding search by click

--- src/app/components/Content/Content.jsx
+++ src/app/components/Content/Content.jsx
@@ -4,12 +4,29 @@ import { ShowPokemon } from "../ShowPokemon/ShowPokemon";
 import styles from "./Content.module.css";
 
 export function Content() {
-  const [pokemonName, setPokemonName] = useState("");
+  const [pokemonName, setPokemonName] = useState("pikachu");
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <section className={styles.content}>
       <ShowPokemon pokemonName={pokemonName} />
-      <input className={styles.input} type="text" />
+      <input
+        className={styles.input}
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <button
+        className={styles.button}
+        onClick={() => {
+          setPokemonName(inputValue.toLowerCase());
+          setInputValue("");
+        }}
+      >
+        Search
+      </button>
     </section>
   );
 }
--- src/app/components/ShowPokemon/ShowPokemon.jsx
+++ src/app/components/ShowPokemon/ShowPokemon.jsx
@@ -7,29 +7,26 @@ import { InfoBox } from "../InfoBox/InfoBox";
 export function ShowPokemon({ pokemonName }) {
   const [pokemonData, setPokemonData] = useState([{}]);
 
-  useEffect(
-    (pokemonName) => {
-      const fetchPokemonData = async () => {
-        try {
-          const response = await axios.get(
-            "https://pokeapi.co/api/v2/pokemon/25"
-          );
-          setPokemonData(response.data);
-        } catch (error) {
-          console.error("Error fetching Pokemon data:", error);
-        }
-      };
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+        );
+        setPokemonData(response.data);
+      } catch (error) {
+        console.error("Error fetching Pokemon data:", error);
+      }
+    };
 
-      fetchPokemonData();
-    },
-    [pokemonName]
-  );
+    fetchPokemonData();
+  }, [pokemonName]);
 
   return (
     <div className={styles.pokemon}>
       <h1>{pokemonData.name}</h1>
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonData.id}.png`}
         alt="pokemon"
       />
       <div className={styles.pokemonInfo}>
